46ab3e34bd65964dec99a9eeb2951ab396c8a44e
Alberto <al_loki@msn.com>
2017-03-09 14:36:21 +0100

    added dialog windows

--- app/src/main/java/com/albert/demopopup1/MainActivity.java
+++ app/src/main/java/com/albert/demopopup1/MainActivity.java
@@ -1,10 +1,13 @@
 package com.albert.demopopup1;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -13,15 +16,29 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView iv;
+    Dialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        showDialog();
+
+        new CountDownTimer(2000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+                showDialog();
+            }
+        }.start();
+
     }
 
-    Dialog dialog;
+
+
 
     private void showDialog() {
         // custom dialog
@@ -30,23 +47,22 @@ public class MainActivity extends AppCompatActivity {
         final Integer[] imagenesID =
                 {R.drawable.bocasecaman, R.drawable.candycrush, R.drawable.chikito,};
 
-        final ImageView iv = (ImageView) findViewById(R.id.popup_image);
-
-        final int resource = imagenesID[rGeneratorImg.nextInt(imagenesID.length)];
-        iv.setImageResource(resource);
 
         dialog = new Dialog(this);
         dialog.setContentView(R.layout.popup1);
+        iv = (ImageView) dialog.findViewById(R.id.popup_image);
 
         // set the custom dialog components - text, image and button
+        final int resourceImg = imagenesID[rGeneratorImg.nextInt(imagenesID.length)];
+        iv.setImageResource(resourceImg);
         ImageButton close = (ImageButton) dialog.findViewById(R.id.btnClose);
+
 
         // Close Button
         close.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dialog.dismiss();
-                //TODO Close button action
             }
         });
 
@@ -58,14 +74,3 @@ public class MainActivity extends AppCompatActivity {
 
 }
 
-/*new CountDownTimer(20000, 1000) {
-
-    public void onTick(long millisUntilFinished) {
-        mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);
-    }
-
-    public void onFinish() {
-        mTextField.setText("done!");
-    }
-}.start();
-*/
